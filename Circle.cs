86c889360f1aa9a33fe641f3f32dc19b52a2dbd1
Aaliyah Kayum <kayumaaliyah@gmail.com>
2022-02-14 09:06:44 -0500

    Add project files.

---
+++ Circle.cs
@@ -0,0 +1,40 @@
+﻿//public Circle (double radius)
+//public double CalculateCircumference()
+//public string CalculateFormattedCircumference()
+//public double CalculateArea()
+//public string CalculateFormattedArea()  
+//private string FormatNumber(double x)
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleObjects
+{
+    internal class Circle
+    {
+        public double radius { get; set; }
+
+        public double CalculateCircumference()
+        {
+        return 2 * mathPI * radius
+            }
+    public double CalculateArea()
+        {
+            return mathPI * radius * 2
+        }
+string choice = Console.ReadLine(); 
+        if(choice == "y")
+{
+    continue;
+}
+else if (choice == "n")
+{
+    break;
+}
+else
+{
+    Console.WriteLine($"Goodbye. You created {circle} Circle object(s)");
+}
---
+++ CircleObjects/Program.cs
@@ -0,0 +1,22 @@
+﻿using CircleObjects;
+
+Console.WriteLine("Welcome to the Circle Tester");
+
+Circle circle = new Circle();
+
+Console.WriteLine("Enter radius: ");
+double r;
+
+while(!int.TryParse(circle.Radius, out r))
+{
+    Console.WriteLine("Invalid Input");
+}
+
+circle.Radius = r;
+
+Console.WriteLine($"The circumference of the circle is {circle.Circumference}");
+
+Console.WriteLine($"The area of the circle is {circle.Area}");
+
+Console.WriteLine("Continue? (y/n)");
+  
